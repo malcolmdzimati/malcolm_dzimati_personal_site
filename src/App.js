91bb8d2a6472ab39b0c3141b9e195b31c0d6b9ba
malcolmdzimati <malcolmdzimati@gmail.com>
2022-07-07 23:25:23 +0200

    :sparkles::zap: added test code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './TodoList';
+import React from 'react';
 
 function App() {
-    return null
+    return ( 
+        <TodoList /> 
+    )
 }
 
 export default App;
