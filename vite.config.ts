61816449ef24d6e4449936b2c234440e6a7ef167
malcolmdzimati <malcolmdzimati@gmail.com>
2025-03-09 11:12:52 +0200

    chore: fix typo in input path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/malcolm_dzimati_personal_site/', 
+  base: './',  
+  build: {
+    outDir: 'dist',  
+  }
 });
