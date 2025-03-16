77365081554340bca11dbfa9e14003dc8baba14d
malcolmdzimati <malcolmdzimati@gmail.com>
2025-03-16 12:16:35 +0200

    chore: fix for routing

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+  basePath: '/malcolm_dzimati_personal_site',
+  assetPrefix: '/malcolm_dzimati_personal_site/', // Note the trailing slash
+};
