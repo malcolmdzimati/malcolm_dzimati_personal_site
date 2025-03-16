6b1c0ea4a8db63d75edc8a34c93a90a2b02c3f96
malcolmdzimati <malcolmdzimati@gmail.com>
2025-03-16 11:42:29 +0200

    chore: moving to pnpm

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"EvNjKTVgIVSO5T2NwkiYx6xH1NnMMTq/TQbT78DX33E=\"\n}"
---
+++ .next/static/chunks/app_page_tsx_c07ced._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_c07ced._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_c07ced._.js",
+  "chunks": [
+    "static/chunks/node_modules_a6abe1._.js",
+    "static/chunks/app_page_tsx_9fdd4f._.js",
+    "static/chunks/node_modules_@heroui_dom-animation_dist_index_mjs_0ade0f._.js"
+  ],
+  "source": "dynamic"
+});
