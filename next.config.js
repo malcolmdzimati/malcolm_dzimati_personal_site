b1e6e6c1cb43eda26b9e7769effc426492c7f657
malcolmdzimati <malcolmdzimati@gmail.com>
2025-03-16 12:27:40 +0200

    chore: fix for static

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  output: 'export',
   basePath: '/malcolm_dzimati_personal_site',
   assetPrefix: '/malcolm_dzimati_personal_site/', // Note the trailing slash
 };
