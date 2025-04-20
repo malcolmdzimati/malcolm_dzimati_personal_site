a27edbca84168c846df3cb4bb39d9e4eebde0c80
malcolmdzimati <malcolmdzimati@gmail.com>
2025-04-20 17:03:01 +0200

    chore: changing reactiveness of the navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,13 +7,11 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@heroui/navbar";
-import { Button } from "@heroui/button";
-import { Kbd } from "@heroui/kbd";
 import { Link } from "@heroui/link";
-import { Input } from "@heroui/input";
 import { link as linkStyles } from "@heroui/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
+import { useState } from "react";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
@@ -25,6 +23,9 @@ import {
 } from "@/components/icons";
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const onOpen = () => setIsOpen(true);
+  const onClose = () => setIsOpen(false);
   return (
     <HeroUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -81,7 +82,7 @@ export const Navbar = () => {
             <GithubIcon className="text-default-500" />
           </Link>
         <ThemeSwitch />
-        <NavbarMenuToggle />
+        <NavbarMenuToggle onPress={isOpen ? onClose : onOpen} />
       </NavbarContent>
 
       <NavbarMenu>
@@ -89,9 +90,10 @@ export const Navbar = () => {
           {siteConfig.navMenuItems.map((item) => (
             <NavbarMenuItem key={`${item.label}`}>
               <Link
-                color={ "foreground"}
+                color="foreground"
                 href={item.href}
                 size="lg"
+                onPress={onClose}
               >
                 {item.label}
               </Link>
