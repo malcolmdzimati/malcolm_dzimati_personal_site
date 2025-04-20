bc12eb48fcebccd7dda102b8600948869048ad11
malcolmdzimati <malcolmdzimati@gmail.com>
2025-04-20 17:48:49 +0200

    chore: changing logo

--- components/icons.tsx
+++ components/icons.tsx
@@ -9,18 +9,19 @@ export const Logo: React.FC<IconSvgProps> = ({
   ...props
 }) => (
   <svg
+    className="h-10 w-10"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 1024 1024"
     fill="none"
     height={size || height}
-    viewBox="0 0 32 32"
     width={size || width}
     {...props}
   >
-    <path
-      clipRule="evenodd"
-      d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-      fill="currentColor"
-      fillRule="evenodd"
-    />
+    {/* Circle background */}
+    <circle cx="512" cy="512" r="512" fill="#000" />
+    {/* Overlapping B and D */}
+    <text x="50%" y="50%" textAnchor="middle" fill="#fff" fontSize="400" fontFamily="Arial, sans-serif" dy=".35em">B</text>
+    <text x="50%" y="50%" textAnchor="middle" fill="#fff" fontSize="400" fontFamily="Arial, sans-serif" dy=".35em" opacity="0.7">D</text>
   </svg>
 );
 
